f58c9f6d2f9b153cc757945ebfda55de9ff8fd8a
Kyrylo Storozhenko <busterakel@gmail.com>
2024-04-01 20:10:00 +0300

    сhore:fixes according to flake

--- payment_system/urls.py
+++ payment_system/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from payment_system.views import (
-    PaymentViewSet
-)
+from payment_system.views import PaymentViewSet
 
 router = routers.DefaultRouter()
 router.register("", PaymentViewSet, basename="payments")
