2b63eaaf7e9cdef024559b7a10da42a323ecff3e
juliastetsko <yulkastetsko@gmail.com>
2024-04-01 13:27:15 +0300

    refactor: add class Covers

--- books_service/models.py
+++ books_service/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
+    class Covers(models.TextChoices):
+        HARD = "hard" "HARD"
+        SOFT = "soft" "SOFT"
+
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=10, choices=Covers.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
 
