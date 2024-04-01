c50b200d54570138a4fccd0790e8b7473d762ee3
juliastetsko <yulkastetsko@gmail.com>
2024-04-01 20:06:57 +0300

    fix: borrowing filter

--- borrowings/models.py
+++ borrowings/models.py
@@ -7,16 +7,19 @@ from library_drf import settings
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
-    is_active = models.BooleanField(default=True)
+
+    @property
+    def is_active(self):
+        return self.actual_return_date is None
 
     def __str__(self):
         return (
             f"Borrow Date: {self.borrow_date}, "
             f" Expected Return Date: {self.expected_return_date}, "
             f"Actual Return Date: {self.actual_return_date}"
-            )
+        )
