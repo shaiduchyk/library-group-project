eca28f7a649a8b5dd2242c9e45b5431529e64205
Kyrylo Storozhenko <busterakel@gmail.com>
2024-04-01 16:35:28 +0300

    chore: added path for user

--- library_drf/urls.py
+++ library_drf/urls.py
@@ -20,8 +20,12 @@ from django.contrib import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "api/books/",
-        include("books_service.urls", namespace="books_service")
-    ),
+    path("api/books/",
+         include("books_service.urls",
+                 namespace="books_service")
+         ),
+    path("api/user/",
+         include("user.urls",
+                 namespace="user")
+         ),
 ]
