f19fc2dfdd0466981697d2ca5afea30d19c6e2fb
juliastetsko <yulkastetsko@gmail.com>
2024-04-01 15:24:56 +0300

    feature: added permission_classes to Borrowing views and filter is_active

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Borrowing
 from .serializers import DetailedBookSerializer
 
@@ -6,8 +8,27 @@ from .serializers import DetailedBookSerializer
 class BorrowingListView(generics.ListAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = DetailedBookSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        user_id = self.request.query_params.get("user_id")
+        is_active = self.request.query_params.get("is_active", None)
+
+        if user.is_superuser and user_id:
+            queryset = Borrowing.objects.filter(user_id=user_id)
+        elif user.is_superuser:
+            queryset = Borrowing.objects.all()
+        else:
+            queryset = Borrowing.objects.filter(user=user)
+
+        if is_active is not None:
+            queryset = queryset.filter(is_active=is_active)
+
+        return queryset
 
 
 class BorrowingDetailView(generics.RetrieveAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = DetailedBookSerializer
+    permission_classes = (IsAuthenticated,)
