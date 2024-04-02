0cc9d0a7b95fd564c9d648c34dd94ec0ef061533
Kyrylo Storozhenko <busterakel@gmail.com>
2024-04-02 15:24:47 +0300

    feature in progress: created func test_payment_session + updated requirements.txt

---
+++ payment_system/stripe_services.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+import stripe
+
+load_dotenv()
+
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+
+
+def create_test_payment_session():
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "usd",
+                "product_data": {
+                    "name": "Test Book borrowing",
+                },
+                "unit_amount": 10000,
+            },
+            "quantity": 1,
+        }],
+        mode="payment",
+        success_url="https://example.com/success",
+        cancel_url="https://example.com/cancel",
+    )
+    return session
+
+print(create_test_payment_session())
