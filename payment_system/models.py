5b448ad35c6b5f619f5e79dd13db3689e5e7353c
Kyrylo Storozhenko <busterakel@gmail.com>
2024-04-02 17:27:34 +0300

    chore: deleted function for calculating borrowing amount in Payment model

--- payment_system/models.py
+++ payment_system/models.py
@@ -28,16 +28,6 @@ class Payment(models.Model):
     session_id = models.CharField(max_length=60, blank=True, null=True)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def save(self, *args, **kwargs):
-        if self.pk is None:
-            self.money_to_pay = self.calculate_total_borrowing_amount()
-        super().save(*args, **kwargs)
-
-    def calculate_total_borrowing_amount(self):
-        borrowing_days = (self.borrowing.expected_return_date - self.borrowing.borrow_date).days
-        total_amount = self.borrowing.book.daily_fee * borrowing_days
-        return total_amount
-
     def __str__(self) -> str:
         return (
             f"|Status: {self.status} | "
