c7c0970705ad2e1c2718e95eec49274bfb145abf
Kyrylo Storozhenko <busterakel@gmail.com>
2024-04-03 12:35:20 +0300

    minor fixes: added BOT_TOKEN to env.sample and deleted token from utils.py

--- borrowings/utils.py
+++ borrowings/utils.py
@@ -1,11 +1,16 @@
 import asyncio
+import os
 
 from telegram import Bot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 def send_telegram_notification(message):
-    bot_token = "7025192077:AAGy6mPZ0YCcVSCy9z9OfL_KGZCJdx6cscI"
-    bot = Bot(token=bot_token)
+    bot = Bot(token=BOT_TOKEN)
 
     chat_id = "-1002006402830"
     asyncio.run(bot.send_message(chat_id=chat_id, text=message))
